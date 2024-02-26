f214abb31e9268e503e933fcace0cabb278a8994
anyquestions69 <danil_tyrenko@icloud.com>
2024-02-26 14:21:35 +0300

    upd

--- static/js/shop.js
+++ static/js/shop.js
@@ -1,7 +1,6 @@
 $(document).ready(async ()=>{
     let products = await fetch('/api/product')
     let itemList = await products.json()
-    console.log(itemList)
     $('#product_list').empty()
     for(let item of itemList.Products){
         $('#product_list').append(`
@@ -57,5 +56,57 @@ $(document).ready(async ()=>{
     if(itemList.currentPage>0){
         $('#pagination').append(`<span>...</span><a href="?page=${itemList.totalPages}">${itemList.totalPages}</a>`)
     }
+    $('#showingPages').text(`Показано ${itemList.Products.length} из ${itemList.totalItems}`)
+    let brands = await fetch('/api/brand')
+    let brandList = await brands.json()
+    $('#brandList').empty()
+    for(let brand of brandList){
+        $('#brandList').append(`<li><a href="#">${brand.name}</a></li>`)
+    }
+
+    $('#productSearchForm').on('submit', async function(e){
+        e.preventDefault()
+        let name = $('#searchInput').val()
+        let products = await fetch('/api/product?name='+name)
+    let itemList = await products.json()
+    $('#product_list').empty()
+    for(let item of itemList.Products){
+        $('#product_list').append(`
+        <div class="product__item col-lg-4 col-md-6">
+                        <div class="product__item__pic set-bg" data-setbg="../img/product/product-1.jpg">
+                           
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Сравнить</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>${item.name}</h6>
+                            <a href="#" class="add-cart">+ Добавить в корзину</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>${item.price} р</h5>
+                            <div class="product__color__select">
+                                <label for="pc-1">
+                                    <input type="radio" id="pc-1">
+                                </label>
+                                <label class="active black" for="pc-2">
+                                    <input type="radio" id="pc-2">
+                                </label>
+                                <label class="grey" for="pc-3">
+                                    <input type="radio" id="pc-3">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+        `)
+    }
+    })
    
-})
+}) 
